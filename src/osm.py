376082e7d8fde72037fcf4052f7ee3ebc023dde9
Louis Adamian <louis@adamian.net>
2025-03-18 18:47:11 -0400

    adds OSM building query

--- src/osm.py
+++ src/osm.py
@@ -25,7 +25,8 @@ def __coords_list_to_str(coords_list: [(float, float)]) -> str:
         coord_str += str(coord[0]) + " " + str(coord[1])
     return coord_str
 
-def get_points(filepath="umb_point_data.geojson"):
+
+def get_points(filepath="umb_point_data.geojson") -> dict:
     """
     gets all the points on UMB campus from OSM
     :param filepath: geojson file path to read from or write to
@@ -34,28 +35,46 @@ def get_points(filepath="umb_point_data.geojson"):
     if os.path.isfile(filepath):
         with open(filepath, "r") as f:
             return json.loads(f.read())
-    query = 'node(poly: "' + __coords_list_to_str(UMB_REGION) + '");'
+    query = 'node["entrance"](poly: "' + __coords_list_to_str(UMB_REGION) + '");'
     json_data = api.get(query)
     with open(filepath, "w") as f:
         f.write(json.dumps(json_data))
     return json_data
 
 
-def get_ways(filepath="umb_way_data.geojson")->dict:
+def get_buildings(filepath="umb_building_data.geojson") -> dict:
+    """
+    gets the polygon outlines of buildings from OSM
+    :param filepath: geojson file path to read from or write to
+    :return: dict of geojson building polygons
+    """
+    if os.path.isfile(filepath):
+        with open(filepath, "r") as f:
+            return json.loads(f.read())
+    query = (
+        'way["building"](poly: "' + __coords_list_to_str(UMB_REGION) + '");(._;>;);out;'
+    )
+    json_data = api.get(query)
+    with open(filepath, "w") as f:
+        f.write(json.dumps(json_data))
+    return json_data
+
+
+def get_ways(filepath="umb_way_data.geojson") -> dict:
     """
     imports a dictionary of OpenStreetMap ways either from a locally cached file or from the Overpass API
     :param filepath: geojson file path to read from or write to
     :return: dictionary of geoJSON data of pedestrian paths
     """
-    query = 'way["highway"~"pedestrian|footway|steps|sidewalk|cycleway"](poly: "' + __coords_list_to_str(UMB_REGION) + '");(._;>;);out;'
-    print(query)
     if os.path.isfile(filepath):
         with open(filepath, "r") as f:
             return json.loads(f.read())
-
+    query = (
+        'way["highway"~"pedestrian|footway|steps|sidewalk|cycleway"](poly: "'
+        + __coords_list_to_str(UMB_REGION)
+        + '");(._;>;);out;'
+    )
     json_data = api.get(query)
     with open(filepath, "w") as f:
         f.write(json.dumps(json_data))
     return json_data
-
-get_points()
