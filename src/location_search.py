e2c66b9b40a7ed75ea7417e789cc9b4b7cca8bc0
Louis Adamian <louis@adamian.net>
2025-04-30 12:20:34 -0400

    update search

--- src/location_search.py
+++ src/location_search.py
@@ -10,7 +10,11 @@ def location_search(query: str):
         f"https://nominatim.openstreetmap.org/search.php?q={query}&viewbox=-71.06081%2C42.32107%2C-71.03228%2C42.31029&bounded=1&format=jsonv2",
         headers=headers,
     )
-    return res.json()[0]["osm_id"], res.json()[0]
+    ids =[]
+    for result in res.json():
+        ids.append(result["osm_id"])
+    return ids
+    # return res.json()["osm_id"], res.json()
 
 
 def get_entrances(id):
@@ -20,13 +24,15 @@ def get_entrances(id):
 
 
 def get_location_nodes(query: str, graph: nx.Graph):
-    id, _ = location_search(query)
-    osm = get_entrances(id)
-    nodes = []
-    for node in osm.nodes:
-        if node.id in graph.nodes:
-            nodes.append(node.id)
-    return nodes
+    ids = location_search(query)
+    for id in ids:
+        osm = get_entrances(id)
+        nodes = []
+        for node in osm.nodes:
+            if node.id in graph.nodes:
+                nodes.append(node.id)
+        if len(nodes) >0:
+            return nodes
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import sys
-
 import networkx as nx
 import osm
 import plotter
@@ -13,11 +12,20 @@ if __name__ == "__main__":
     else:
         start = input("please enter your starting location ")
         end = input("please enter your ending location ")
-    graph = osm.get_graph()
+    graph = osm.get_graph()#(force_download=True)
     start_nodes = get_location_nodes(start, graph)
     end_nodes = get_location_nodes(end, graph)
     print(start_nodes)
     print(end_nodes)
-    # TODO: replace this with dijkstra or A*
-    path = nx.shortest_path(graph, start_nodes[0], end_nodes[0])
+        # TODO: replace this with dijkstra or A*
+    shortest_length = float('inf')
+    start = start_nodes[0]
+    end = end_nodes[0]
+    for start_node in start_nodes:
+        for end_node in end_nodes:
+            len = nx.shortest_path_length(graph, start_node, end_node)
+            if len<shortest_length:
+                shortest_length = len
+                nodes = (start_node,end_node)
+    path = nx.shortest_path(graph, start, end)
     plotter.plot_route(path, graph, crop_to_route=True, show=True)
