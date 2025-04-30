b5eb8334f90666e681261e4d1e9ac1dd54c5b58e
erikasev <severinomd@icloud.com>
2025-04-30 19:44:59 -0400

    resolve conflicts

--- src/main.py
+++ src/main.py
@@ -1,9 +1,70 @@
 import networkx as nx
 import osm
 import plotter
+from algorithms.astar import astar
+from graph import build_osm_graph
+from location_search import get_location_nodes
+from algorithms.dijkstras import dijkstra
+import time
+
 
 if __name__ == "__main__":
-    graph = osm.get_graph()
-    # TODO: replace this with dijkstra or A*
-    path = nx.shortest_path(graph, 7671135318, 12677458704)
+    # Erases the generated pkl file before new compilation
+    try:
+        os.remove("umb_way_data.pkl")
+        os.remove("umb_graph.pkl")
+    except:
+        print("\nUMass Boston CS310 Project\n")
+
+    get_start = input("Input starting point: \n")
+    get_goal = input("\nInput destination: \n")
+
+    algo_choice = input("\nChoose algorithm ('dijkstra', 'astar', or 'both'): \n")
+    graph = build_osm_graph()
+
+    # Query for node from user standard input
+    start_node = get_location_nodes(get_start, graph)
+    if not start_node:
+        raise ValueError(f"Node not found for '{get_start}'")
+
+    goal_node = get_location_nodes(get_goal, graph)
+    if not goal_node:
+        raise ValueError(f"Node not found for '{get_goal}'")
+
+    start_id = start_node[0]
+    goal_id = goal_node[0]
+
+    match algo_choice:
+        case 'dijkstra':
+            print("\nStarting route using Dijkstra algorithm...\n")
+            path = dijkstra(graph, start_id, goal_id)
+        case 'astar':
+            print("\nStarting route using A* algorithm...\n")
+            path = astar(graph, start_id, goal_id)
+        case 'both':
+            print("\nStarting route..\nDijkstra...\n")
+            path_d = dijkstra(graph, start_id, goal_id)
+            time.sleep(1)
+            print("\nA*...\n")
+            path_a = astar(graph, start_id, goal_id)
+
+            plotter.plot_route(path_d, graph, crop_to_route=True, show=True)
+            plotter.plot_route(path_a, graph, crop_to_route=True, show=True)
+            exit(0)
+        case _:
+            raise ValueError("Invalid choice")
+
+    # Calculate actual walking distance in time between the two points
+    walking_distance = 0
+    for i in range(len(path) - 1):
+        walking_distance += graph[path[i]][path[i + 1]]['weight']
+
+    # Wikipedia 'preferred walking speed' 5 km/h ~ 1.39 meters per seconds
+    estimate_walk_speed= 5000 / 60
+    estimate_walk_time = walking_distance / estimate_walk_speed
+    miles = walking_distance * 0.000621371
+
+    print(f"Estimated walk time: {estimate_walk_time:.1f} minutes")
+    print(f"Walking distance: {miles:.2f} miles \n")
+
     plotter.plot_route(path, graph, crop_to_route=True, show=True)
