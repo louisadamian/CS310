7cb1f1c2818fa996fa3879b0bf57aabf05ecbefc
erikasev <68448171+erikasev@users.noreply.github.com>
2025-05-04 12:09:06 -0400

    add astar and dijkstras (#9)
    * Add .gitignore file to ignore environment and temporary files
    
    * second commit
    
    * Add A* structure and graph builder module
    
    * add graph.py
    
    * test astar for correctness
    
    * A* initial commit
    
    * graph builder initial commit
    
    * initial commit
    
    * update readme to inlcude pytest instr
    
    * test astar path
    
    * osm edits for run_astar
    
    * plotter edits for run_astar
    
    * add terminal command for 'run_astar'
    
    * add terminal command for 'run_astar'
    
    * fix great circle distance
    
    * add plotting function from list of nodes in graph with auto-zoom
    
    * fix plotter main function
    
    * cleanup docstrings
    
    * cleanup main
    
    * Revert "Fix conflicts"
    
    * resolve conflicts
    
    * add .DS_Store to .gitignore
    
    * final cleanup
    
    * changes before merging main
    
    * Delete src/umb_graph.pkl
    
    * Delete src/umb_way_data.pkl
    
    * Delete src/graph.py
    
    * Delete src/.gitignore
    
    ---------
    
    Co-authored-by: Louis Adamian <louis@adamian.net>

--- src/main.py
+++ src/main.py
@@ -3,29 +3,37 @@ import networkx as nx
 import osm
 import plotter
 from location_search import get_location_nodes
-
+from algorithms.astar import astar
+from algorithms.dijkstras import dijkstra
 if __name__ == "__main__":
     print(len(sys.argv))
     if len(sys.argv) > 2:
         start = sys.argv[1]
         end = sys.argv[2]
+        algorithm = sys.argv[3]
     else:
         start = input("please enter your starting location ")
         end = input("please enter your ending location ")
+        algorithm = input("please choose an algorithm ('dijkstra', 'astar') ")
     graph = osm.get_graph()
     start_nodes = get_location_nodes(start, graph)
     end_nodes = get_location_nodes(end, graph)
-    print(start_nodes)
-    print(end_nodes)
-    # TODO: replace this with dijkstra or A*
-    shortest_length = float("inf")
-    start = start_nodes[0]
-    end = end_nodes[0]
-    for start_node in start_nodes:
-        for end_node in end_nodes:
-            len = nx.shortest_path_length(graph, start_node, end_node)
-            if len < shortest_length:
-                shortest_length = len
-                nodes = (start_node, end_node)
-    path = nx.shortest_path(graph, start, end)
+    match algorithm:
+        case 'dijkstra':
+            print("\nComputing shortest path using Dijkstra's algorithm...\n")
+            path = dijkstra(graph, start_nodes[0], end_nodes[0])
+        case 'astar':
+            print("\nComputing shortest path using A* algorithm...\n")
+            path = astar(graph, start_nodes[0], end_nodes[0])
+        case _:
+            raise ValueError("Invalid choice")
+    walking_distance = 0
+    for i in range(len(path) - 1):
+        walking_distance += graph[path[i]][path[i + 1]]['weight']
+    estimate_walk_speed = 5000 / 60
+    estimate_walk_time = walking_distance / estimate_walk_speed
+    miles = walking_distance * 0.000621371
+
+    print(f"Estimated walk time: {estimate_walk_time:.1f} minutes")
+    print(f"Walking distance: {miles:.2f} miles \n")
     plotter.plot_route(path, graph, crop_to_route=True, show=True)
